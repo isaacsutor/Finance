501bc01bbe7647f97df3ff85c87169cf1804c961
Isaac Sutor <isaac.sutor.15@cnu.edu>
2019-05-07 22:11:52 -0400

    simple rate of return for stocks

--- RatesofReturn/SimpleReturnsPart1.py
+++ RatesofReturn/SimpleReturnsPart1.py
@@ -4,3 +4,14 @@ import matplotlib.pyplot as plt
 PG = wb.DataReader('PG', data_source='yahoo', start='1995-1-1')
 PG['simple_return'] = (PG['Adj Close'] / PG['Adj Close'].shift(1)) - 1
 print(PG['simple_return'])
+PG['simple_return'].plot(figsize=(8, 5))
+# plt.show()
+
+# mean return of PG
+avg_returns_d = PG['simple_return'].mean()
+print(avg_returns_d)
+
+avg_returns_a = PG['simple_return'].mean() * 250
+print(avg_returns_a)
+
+print(str(round(avg_returns_a, 5) * 100) + ' %')
